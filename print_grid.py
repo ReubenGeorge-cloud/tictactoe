4253cb0531af4a2ae7c8607d70bd0c6610365310
Reuben <reubengeorge101@gmail.com>
2021-05-18 23:38:00 +0530

    grid_checker

--- print_grid.py
+++ print_grid.py
@@ -1,9 +1,30 @@
 class grid:
-    def __init__(self,arr):
-       self.arr = arr
-       self.x = " xx      xx \n   xx  xx   \n     xx     \n   xx  xx   \n xx      xx "
-       self.o = "    oooo    \n  oo    oo  \n oo      oo \n  oo    oo  \n    oooo    "
-       self.none = "            \n            \n            \n            \n            "
-       self.vline = "|\n|\n|\n|\n|"
-       self.hline = "-------------|-------------|-------------"
+   def __init__(self,arr):
+      self.arr = arr
+      self.x = [" xx      xx ","   xx  xx   ","     xx     ","   xx  xx   "," xx      xx "]
+      self.o = ["    oooo    ","  oo    oo  "," oo      oo ","  oo    oo  ","    oooo    "]
+      self.none = ["            ","            ","            ","            ","            "]
+      self.dict = {0:self.none,1:self.x,2:self.y}
+      self.vline = ["|","|","|","|","|"]                                                               #REMOVE?
+      self.hline = "-------------|-------------|-------------"
 
+   def print_grid(self):
+      for i in range(3):
+         for j in range(5):
+            print('|',self.dict(self.arr[i][0])[j],'|',self.dict(self.arr[i][1])[j],'|',self.dict(self.arr[i][2])[j])  #Prints each line in the output 
+         if i < 2:
+            print(self.hline)
+
+   def grid_checker(self):
+      for i in range(3):
+         if (self.arr[i][0] == self.arr[i][1] and self.arr[i][1] == self.arr[i][2]):           
+            return self.arr[i][0]
+         elif (self.arr[0][i] == self.arr[1][i] and self.arr[1][i] == self.arr[2][i]):         
+            return self.arr[0][i]
+      if (self.arr[0][0] == self.arr[1][1] and self.arr[1][1] == self.arr[2][2]):
+         return self.arr[1][1]
+      elif (self.arr[0][0] == self.arr[1][1] and self.arr[1][1] == self.arr[2][2]):           
+         return self.arr[1][1]
+      else:
+         return 4
+            
