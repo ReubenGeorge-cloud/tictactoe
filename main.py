bc19def31c877b744dc0f097576ebe71015a2efc
Reuben George <69674713+ReubenGeorge-cloud@users.noreply.github.com>
2021-05-26 10:10:07 +0530

    comments-final

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from print_grid import grid
 
-arr = [[0,0,0],[0,0,0],[0,0,0]]
-grid = grid(arr)
-grid.print_grid()
+arr = [[0,0,0],[0,0,0],[0,0,0]]                         #store the values of each grid element as 0,1,2
+grid = grid(arr)                                        
+grid.print_grid()                                           #prints the grid with current values from arr
 i = 0
 
 while(i<9):
-    row = int(input("Enter the row name: "))
+    row = int(input("Enter the row name: "))            #taking input
     col = int(input("Enter the column name: "))
-    if(arr[row][col] == 0):
-        if(i%2):
-            arr[row][col] = 1
+    if(arr[row][col] == 0):                             #checking if square is unmarked before marking
+        if(i%2):                                        
+            arr[row][col] = 1                           #marks square with player 1's symbol (x)
         else:
-            arr[row][col] = 2
-        i+=1
+            arr[row][col] = 2                           #marks square with player 2's symbol (o)
+        i+=1                                            #i increments only if the square was unmarked
     else:
-        print("\nThat square has already been marked! Please select another square")
-        continue
-    grid.print_grid()
-    res = grid.grid_checker()
+        print("\nThat square has already been marked! Please select another square") 
+        continue                                        
+    grid.print_grid()           
+    res = grid.grid_checker()                           #grid_checker func to check if a player has won
     if (res == 1):
         print("\nPlayer 1 wins the game!")
         break
     elif(res == 2):
         print("\nPlayer 2 wins the game!")
         break
-    elif(i == 9):
+    elif(i == 9):                                       #draws and ends the game when all squares have been marked
         print("\nThe game has ended in a draw!")
         
